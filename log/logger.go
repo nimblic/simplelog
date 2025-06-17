3f41ab3c27b3159c238946151c7d669658236cbd
satchit puthenveetil <sputhenveetil@medtasker.com>
2025-06-17 22:51:46 +1000

    MT-8394 add support for slog based logger

---
+++ log/logger.go
@@ -0,0 +1,143 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+	"sync"
+)
+
+const (
+	LevelNotice  = slog.Level(2)
+	LevelVerbose = slog.Level(-5)
+)
+
+var LevelNames = map[slog.Leveler]string{
+	LevelVerbose: "VERBOSE",
+	LevelNotice:  "NOTICE",
+}
+
+type MedLog struct {
+	*slog.Logger
+}
+
+var loggerSingleton *MedLog
+var once sync.Once
+
+func Init(level string, usePrettyPrintLogger bool) *MedLog {
+	var defaultLogLevel slog.Level
+	switch strings.ToLower(level) {
+	case "verbose":
+		defaultLogLevel = LevelVerbose
+	case "debug":
+		defaultLogLevel = slog.LevelDebug
+	case "info":
+		defaultLogLevel = slog.LevelInfo
+	case "notice":
+		defaultLogLevel = LevelNotice
+	case "warning":
+		defaultLogLevel = slog.LevelWarn
+	case "error":
+		defaultLogLevel = slog.LevelError
+	}
+	once.Do(func() {
+		opts := &slog.HandlerOptions{
+			// AddSource: true,
+			Level: defaultLogLevel,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if a.Key == slog.LevelKey {
+					level := a.Value.Any().(slog.Level)
+					levelLabel, exists := LevelNames[level]
+					if !exists {
+						levelLabel = level.String()
+					}
+
+					a.Value = slog.StringValue(levelLabel)
+				}
+
+				return a
+			},
+		}
+		var logger *slog.Logger
+		if usePrettyPrintLogger {
+			logger = slog.New(NewHandler(opts))
+		} else {
+			logger = slog.New(slog.NewJSONHandler(os.Stderr, opts))
+		}
+		loggerSingleton = &MedLog{logger}
+	})
+
+	return loggerSingleton
+}
+
+func Verbose(msg string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Log(context.TODO(), LevelVerbose, msg, args...)
+	}
+}
+
+func Verbosef(format string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Log(context.TODO(), LevelVerbose, fmt.Sprintf(format, args...))
+	}
+}
+
+func Debug(msg string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Debug(msg, args...)
+	}
+}
+
+func Debugf(format string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Debug(fmt.Sprintf(format, args...))
+	}
+}
+
+func Info(msg string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Info(msg, args...)
+	}
+}
+func Infof(format string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Info(fmt.Sprintf(format, args...))
+	}
+}
+
+func Notice(msg string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Log(context.TODO(), LevelNotice, msg, args...)
+	}
+}
+
+func Noticef(format string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Log(context.TODO(), LevelNotice, fmt.Sprintf(format, args...))
+	}
+}
+
+func Warning(msg string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Warn(msg, args...)
+	}
+}
+func Warningf(format string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Warn(fmt.Sprintf(format, args...))
+	}
+}
+
+func Error(msg string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Error(msg, args...)
+	}
+}
+
+func Errorf(format string, args ...any) {
+	if loggerSingleton != nil {
+		loggerSingleton.Error(fmt.Sprintf(format, args...))
+	}
+}
